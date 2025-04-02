216f313ed11a08fe6b658f1485a6a6d9fa1c7da4
danielendaylalu1 <endaylaludaniel@gmail.com>
2025-04-02 22:49:22 +0300

    learned adding and removing from list

--- lists/list.py
+++ lists/list.py
@@ -5,3 +5,13 @@ print(myList[:2]) # output ['python', 'c++']
 
 myList[1] = "c#"
 print(myList) # output ['python', 'c#', 'javascript']
+
+# To add an item to the end of the list
+myList.append("go")
+
+print(myList) # output ['python', 'c#', 'javascript', 'go']
+
+#If there are more than one item with the specified value, the remove() method removes the first occurrence:
+myList.remove("c#")
+
+print(myList) # output ['python', 'javascript', 'go']
