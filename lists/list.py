8e0dfd17e114a9577a120fbcb762b054ba2064c9
danielendaylalu1 <endaylaludaniel@gmail.com>
2025-04-02 22:44:02 +0300

    learned lists accessing and mutation

--- lists/list.py
+++ lists/list.py
@@ -1,2 +1,7 @@
 myList = ["python", "c++", "javascript"]
 print(myList) # output ["python", "c++", "javascript"]
+print(myList[1]) # output "c++"
+print(myList[:2]) # output ['python', 'c++']
+
+myList[1] = "c#"
+print(myList) # output ['python', 'c#', 'javascript']
