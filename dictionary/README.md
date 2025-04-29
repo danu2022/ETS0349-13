f5e6e8a3b2d39664b6e3c2fa26ebf9f203e90fe8
danielendaylalu1 <endaylaludaniel@gmail.com>
2025-04-29 23:42:31 +0300

    learned ways to remove key value pairs from a dictionary

--- dictionary/README.md
+++ dictionary/README.md
@@ -8,3 +8,10 @@
 
 1.  Adding an item to the dictionary is done by using a new index key and assigning a value to it:
     a. update() a way to update a dictionary by adding a key value pair
+    b. dic["key"] = "value"
+
+2.  Removing Items
+    There are several methods to remove items from a dictionary:
+    a.The pop() method removes the item with the specified key name:
+    b.The popitem() method removes the last inserted item
+    c.The del keyword removes the item with the specified key name:
--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -16,7 +16,26 @@ print(values) # output dict_values(['daniel', 'male', 50])
 # Adding items in to dictionary
 
 thisdict["student"] = True
-print(thisdict) # output {'name': 'daniel', 'sex': 'male', 'age': 50, 'student':True}
+print(thisdict) 
+# output {'name': 'daniel', 'sex': 'male', 'age': 50, 'student':True}
 
-thisdict.update({"acdamicyear": "3rd"})
-print(thisdict) # output {'name': 'daniel', 'sex': 'male', 'age': 50, 'student':True, 'acdamicyear':'3rd'}
+thisdict.update({"acadamicyear": "3rd"}) # adds acadamicyear key value pair from dic
+print(thisdict) 
+# output {'name': 'daniel', 'sex': 'male', 'age': 50, 'student':True, 'acdamicyear':'3rd'}
+
+
+thisdict.update({"age": 40}) # udaptes ages value from 50 to 40
+print(thisdict) 
+# output {'name': 'daniel', 'sex': 'male', 'age': 40, 'student':True, 'acdamicyear':'3rd'}
+
+thisdict.pop("acadamicyear") # removes acadamicyear key value pair from dic
+print(thisdict)
+# output {'name': 'daniel', 'sex': 'male', 'age': 40, 'student':True}
+
+thisdict.popitem() # removes the last key value pair from dic and in this case student
+print(thisdict)
+# output {'name': 'daniel', 'sex': 'male', 'age': 40, }
+
+del thisdict["name"] # removes the last key value pair from dic and in this case name
+print(thisdict)
+# output {'sex': 'male', 'age': 40, }
