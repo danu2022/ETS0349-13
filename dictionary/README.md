23baf8a97a931769fa41763664b1d2ba817a59f6
danielendaylalu1 <endaylaludaniel@gmail.com>
2025-04-29 23:34:40 +0300

    learned how to update dictionary adding and updating

--- dictionary/README.md
+++ dictionary/README.md
@@ -3,3 +3,8 @@
     Dictionaries are used to store data values in key:value pairs.
 
     A dictionary is a collection which is ordered\*, changeable and do not allow duplicates.
+
+# Dictionary manpulation
+
+1.  Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+    a. update() a way to update a dictionary by adding a key value pair
--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -6,3 +6,17 @@ thisdict = {
 
 print(thisdict) # output {'name': 'daniel', 'sex': 'male', 'age': 50}
 print(thisdict["age"]) #output 50
+
+keys = thisdict.keys()
+print(keys) # output dict_keys(['name', 'sex', 'age'])
+
+values = thisdict.values()
+print(values) # output dict_values(['daniel', 'male', 50])
+
+# Adding items in to dictionary
+
+thisdict["student"] = True
+print(thisdict) # output {'name': 'daniel', 'sex': 'male', 'age': 50, 'student':True}
+
+thisdict.update({"acdamicyear": "3rd"})
+print(thisdict) # output {'name': 'daniel', 'sex': 'male', 'age': 50, 'student':True, 'acdamicyear':'3rd'}
